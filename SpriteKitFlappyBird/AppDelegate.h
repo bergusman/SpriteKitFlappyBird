e01ee270c9ffda236835bca994fd879d59830228
Vitaly Berg <bergusman@gmail.com>
2014-04-30 01:14:56 +0400

    Initial Commit

---
+++ SpriteKitFlappyBird/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitFlappyBird
+//
+//  Created by Vitaly Berg on 30/04/14.
+//  Copyright (c) 2014 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpriteKitFlappyBirdTests/SpriteKitFlappyBirdTests.m
@@ -0,0 +1,34 @@
+//
+//  SpriteKitFlappyBirdTests.m
+//  SpriteKitFlappyBirdTests
+//
+//  Created by Vitaly Berg on 30/04/14.
+//  Copyright (c) 2014 Vitaly Berg. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SpriteKitFlappyBirdTests : XCTestCase
+
+@end
+
+@implementation SpriteKitFlappyBirdTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
